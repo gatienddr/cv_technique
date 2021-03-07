f4a98e07b4f8d65a89438b13467002dd4147b7ce
Gatien Didry <gatien.didry@etudiant.univ-lr.fr>
2021-03-07 20:03:39 +0100

    page d'accueil fini

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# cv_technique
+# Portofolio de Gatien
+
+## Description
+  Ce projet est le développement de mon portofolio en ligne.
+  
+## Technologie utilisée
+ Flutter
+ 
+ ## Git 
+  git flow utilisé
+
+## Production 
+  La version en production de mon Portofolio se trouvera à l'adresse gatien-dev.fr
+  
+ 
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,19 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Portofolio technique Gatien',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-          fontFamily: 'Ubuntu'
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'NotoSans'),
       home: MyHomePage(title: 'Mon Portofolio technique'),
     );
   }
@@ -31,15 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -47,60 +29,85 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
-        Expanded(child:  Image(image: AssetImage('../img/photoCv.jpg'))),
-            Text(
-              'Ce jeune est motivé à travailler et intégrer pleinement vos équipes',
+            Expanded(child: Image(image: AssetImage('../img/photoCv.jpg'))),
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              constraints: BoxConstraints.expand(
+                height: MediaQuery.of(context).size.width * 0.1,
+                width: MediaQuery.of(context).size.width * 0.8,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Ce jeune est motivé à travailler et à intégrer pleinement vos équipes',
+                    style: TextStyle(
+                        color: Colors.white,
+                        //height:MediaQuery.of(context).size.width*0.001,
+                        fontSize: MediaQuery.of(context).size.width * 0.01),
+                  ),
+                  Text(
+                    'Bienvenue sur mon portofolio technique ',
+                    style: TextStyle(
+                        color: Colors.white,
+                        height: 1.9,
+                        fontSize: MediaQuery.of(context).size.height *
+                            MediaQuery.of(context).size.width *
+                            0.00005),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: Colors.brown[400],
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10.0),
+                    topRight: Radius.circular(10.0),
+                    bottomLeft: Radius.circular(10.0),
+                    bottomRight: Radius.circular(10.0)),
+              ),
             ),
-
+            Container(
+              constraints: BoxConstraints.expand(
+                height: Theme.of(context).textTheme.headline4.fontSize * 1.1 +
+                    200.0,
+              ),
+              padding: const EdgeInsets.all(8.0),
+              color: Colors.blueGrey[400],
+              alignment: Alignment.center,
+              child: Column(children: [
+                Text(
+                  "Vous pouvez y trouver mes compétences techniques et aptitudes",
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height *
+                          MediaQuery.of(context).size.width *
+                          0.00002,
+                      height: 1.5,
+                      color: Colors.white),
+                ),
+                InkWell(
+                  child: Text("N'hésitez pas à me contacter en cliquant ici"),
+                  onTap: () async {
+                    await launch("mailto:gassddr@gmail.com");
+                  },
+                ),
+                Text(
+                  "110110101100101 111001001100011 1101001",
+                  style:
+                      TextStyle(fontSize: 10.0, height: 5, color: Colors.white),
+                ),
+                Text(
+                  "6120 6269 656E 746F 74",
+                  style: TextStyle(fontSize: 10.0, color: Colors.white),
+                )
+              ]),
+              transform: Matrix4.rotationZ(0.1),
+            )
           ],
         ),
       ),
